3fc7f3b2ff6a8a4eaba2579eec0f4b9ba7d88628
Arie <arie@wego.com>
2013-07-16 23:07:26 +0700

    init

---
+++ netraAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  netraAppDelegate.h
+//  smooke
+//
+//  Created by Arie on 7/16/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface netraAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
